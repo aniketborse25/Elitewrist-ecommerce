d1f96bc526ddbb6f18e197b64bc9418463ecdac2
aniketborse25 <aniketborse2003@gmail.com>
2026-06-29 10:31:04 +0530

    feat: add contact us page and footer link

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -26,7 +26,7 @@ const Contact = () => {
                 name: "", email: "", phone: "", message: "",
             });
         } catch (error) {
-            console.log(error);
+            console.log(error.response.data);
 
             alert(
                 error?.response?.data?.message || "Something went to wrong"
